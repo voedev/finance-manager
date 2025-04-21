e6394440c1d0d8d806f20a6e460810bdab78d93c
Artem Voevoda <voevodacode@gmail.com>
2025-04-21 13:04:18 +0300

    typo

--- src/main/java/com/voedev/finance/model/entity/BaseEntity.java
+++ src/main/java/com/voedev/finance/model/entity/BaseEntity.java
@@ -2,7 +2,7 @@ package com.voedev.finance.model.entity;
 
 import java.io.Serializable;
 
-// todo need update
+// todo need update, add optimistic lock (why)
 public interface BaseEntity<T extends Serializable> {
 
     T getId();
