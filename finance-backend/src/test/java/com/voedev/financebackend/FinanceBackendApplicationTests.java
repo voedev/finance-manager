f734f267cd45dba038c5864a5102c7be0fdc7f80
Artem Voevoda <voevodacode@gmail.com>
2025-04-22 11:32:59 +0300

    FinanceBackendApplicationTests

--- finance-backend/src/test/java/com/voedev/financebackend/FinanceBackendApplicationTests.java
+++ finance-backend/src/test/java/com/voedev/financebackend/FinanceBackendApplicationTests.java
@@ -1,13 +1,10 @@
 package com.voedev.financebackend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FinanceBackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
