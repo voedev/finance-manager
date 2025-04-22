a48b1f020d3b5e5f74fcfb755cab2b14f9b4d055
Artem Voevoda <voevodacode@gmail.com>
2025-04-22 10:43:25 +0300

    notification module init

---
+++ notification-sender/src/main/java/com/voedev/notificationsender/NotificationSenderApplication.java
@@ -0,0 +1,13 @@
+package com.voedev.notificationsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSenderApplication.class, args);
+	}
+
+}
