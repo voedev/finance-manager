6039dd55ae7d53991839b5aa79f7eed91e9cd346
Artem Voevoda <voevodacode@gmail.com>
2025-05-03 15:00:10 +0300

    fix NotificationSenderApplicationTests

--- notification-sender/src/test/java/com/voedev/notificationsender/NotificationSenderApplicationTests.java
+++ notification-sender/src/test/java/com/voedev/notificationsender/NotificationSenderApplicationTests.java
@@ -3,7 +3,6 @@ package com.voedev.notificationsender;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class NotificationSenderApplicationTests {
 
 	@Test
