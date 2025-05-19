8c462816a677e982c7e1157f40b92f51e6a0ba59
Artem Voevoda <voevodacode@gmail.com>
2025-05-19 19:05:34 +0300

    draft

---
+++ finance-backend/src/main/java/com/voedev/financebackend/model/dto/account/request/UpdateAccountRequest.java
@@ -0,0 +1,30 @@
+package com.voedev.financebackend.model.dto.account.request;
+
+import com.voedev.financebackend.validation.currency.ValidCurrency;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateAccountRequest {
+
+    @NotBlank(message = "The title field is required.")
+    @Size(min = 2, max = 200, message = "Title must be between 2 and 200 characters")
+    private String title;
+
+    @NotBlank(message = "Currency field cannot be null.")
+    @ValidCurrency
+    private String currency;
+
+    private BigDecimal balance;
+
+    private String status;
+}
