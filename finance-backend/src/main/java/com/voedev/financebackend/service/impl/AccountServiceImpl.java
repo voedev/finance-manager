efa2dba754dc96a16c52c54fac98471273a759a9
Artem Voevoda <voevodacode@gmail.com>
2025-08-03 14:27:59 +0300

    Typo

--- finance-backend/src/main/java/com/voedev/financebackend/service/impl/AccountServiceImpl.java
+++ finance-backend/src/main/java/com/voedev/financebackend/service/impl/AccountServiceImpl.java
@@ -86,6 +86,4 @@ public class AccountServiceImpl implements AccountService {
                     throw new AccountAlreadyExistsException(title, "Account already exists.");
                 });
     }
-
-
 }
