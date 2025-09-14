de35cedb3d3ac1beb58fdfec193a6a844e7c8a6e
Artem Voevoda <voevodacode@gmail.com>
2025-09-14 15:07:21 +0300

    Fix objectMapper

--- finance-backend/src/main/java/com/voedev/financebackend/controller/AccountController.java
+++ finance-backend/src/main/java/com/voedev/financebackend/controller/AccountController.java
@@ -1,34 +1,91 @@
 package com.voedev.financebackend.controller;
 
-import com.voedev.financebackend.model.dto.account.request.CreateAccountRequest;
-import com.voedev.financebackend.model.dto.account.request.UpdateAccountRequest;
-import com.voedev.financebackend.model.dto.account.response.AccountResponse;
-import com.voedev.financebackend.service.AccountService;
-import jakarta.validation.Valid;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.voedev.financebackend.model.entity.Account;
+import com.voedev.financebackend.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
-@PreAuthorize("hasAnyRole('ADMIN','USER')")
 @RequiredArgsConstructor
 public class AccountController {
 
-    private final AccountService accountService;
+    private final AccountRepository accountRepository;
+    private final ObjectMapper objectMapper;
 
-    @PostMapping("/create")
-    @PreAuthorize("hasAuthority('WRITE_PRIVILEGE') and hasAnyRole('ADMIN','USER')")
-    public ResponseEntity<AccountResponse> create(@Valid @RequestBody CreateAccountRequest request) {
-        AccountResponse accountResponse = accountService.create(request);
-        return ResponseEntity.ok().body(accountResponse);
+    @PostMapping
+    public Account create(@RequestBody Account account) {
+        return accountRepository.save(account);
     }
 
-    @PutMapping("/update")
-    @PreAuthorize("hasAuthority('UPDATE_PRIVILEGE') and hasAnyRole('ADMIN','USER')")
-    public ResponseEntity<AccountResponse> update(@Valid @RequestBody UpdateAccountRequest request) {
-        AccountResponse accountResponse = accountService.update(request);
-        return ResponseEntity.ok().body(accountResponse);
+
+    @GetMapping
+    public PagedModel<Account> getAll(@ParameterObject Pageable pageable) {
+        Page<Account> accounts = accountRepository.findAll(pageable);
+        return new PagedModel<>(accounts);
+    }
+
+    @GetMapping("/{id}")
+    public Account getOne(@PathVariable Long id) {
+        Optional<Account> accountOptional = accountRepository.findById(id);
+        return accountOptional.orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity with id `%s` not found".formatted(id)));
+    }
+
+    @GetMapping("/by-ids")
+    public List<Account> getMany(@RequestParam List<Long> ids) {
+        return accountRepository.findAllById(ids);
+    }
+
+
+    @PatchMapping("/{id}")
+    public Account patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
+        Account account = accountRepository.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity with id `%s` not found".formatted(id)));
+
+        objectMapper.readerForUpdating(account).readValue(patchNode);
+
+        return accountRepository.save(account);
+    }
+
+    @PatchMapping
+    public List<Long> patchMany(@RequestParam List<Long> ids, @RequestBody JsonNode patchNode) throws IOException {
+        Collection<Account> accounts = accountRepository.findAllById(ids);
+
+        for (Account account : accounts) {
+            objectMapper.readerForUpdating(account).readValue(patchNode);
+        }
+
+        List<Account> resultAccounts = accountRepository.saveAll(accounts);
+        return resultAccounts.stream()
+                .map(Account::getId)
+                .toList();
+    }
+
+    @DeleteMapping("/{id}")
+    public Account delete(@PathVariable Long id) {
+        Account account = accountRepository.findById(id).orElse(null);
+        if (account != null) {
+            accountRepository.delete(account);
+        }
+        return account;
+    }
+
+    @DeleteMapping
+    public void deleteMany(@RequestParam List<Long> ids) {
+        accountRepository.deleteAllById(ids);
     }
 }
